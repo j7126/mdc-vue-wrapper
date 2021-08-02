713c6b2d2bb91b42b46dbd46a21d22e1c6cbe77d
Jefferey Neuffer <35882868+j7126@users.noreply.github.com>
2021-08-03 09:58:20 +1000

    Update licence notice

--- src/mdc-vue-wrapper.js
+++ src/mdc-vue-wrapper.js
@@ -1,9 +1,21 @@
 /*
  * mdc-vue-wrapper
  * A basic wrapper to use material components with vuejs
- * (c) 2020 - 2021 Jefferey Neuffer github.com/j7126
- * Released under the GNU Affero General Public License.
- */
+ * Copyright (c) 2020 - 2021 Jefferey Neuffer (github.com/j7126)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 // Top app bar
 Vue.component('mdc-top-app-bar', {
